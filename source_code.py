d5696168c6149d8ca23c3f11c48dbb909cecab54
AarkeTripathi <113425044+AarkeTripathi@users.noreply.github.com>
2022-11-07 06:58:24 +0530

    Add files via upload

--- source_code.py
+++ source_code.py
@@ -1,9 +1,5 @@
-import time
-t1=time.time()
-import numpy
 import cv2
 from deepface import DeepFace
-import matplotlib.pyplot as plt
 
 face_casc=cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
 cap=cv2.VideoCapture(0)
@@ -14,15 +10,14 @@ cap.set(10,150)             #10 is the id for brightness
 
 while True:
     success,img=cap.read()
-    emotion = DeepFace.analyze(img,actions=['emotion'],enforce_detection=False)
+    emotion = DeepFace.analyze(img,actions=['emotion','gender'],enforce_detection=False)
     face=face_casc.detectMultiScale(img,1.1,4)
     for(w,x,y,z) in face:
         cv2.rectangle(img,(w,x),(w+y,x+z),(210,0,210),2)
         font=cv2.FONT_HERSHEY_DUPLEX
         cv2.putText(img,emotion['dominant_emotion'],(w,x-20),font,1,(210,0,210),2)
+        cv2.putText(img, emotion['gender'], (w+150, x-20), font, 1, (210, 0, 210), 2)
     cv2.imshow('Video',img)
     if cv2.waitKey(1) & 0xFF==ord('q'):
         break
 
-t2=time.time()
-print(t2-t1)
